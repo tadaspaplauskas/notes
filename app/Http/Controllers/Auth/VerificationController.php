cf46041bdf9029c9c22fa6b340e0ea77136d5a04
Tadas <tadas@paplauskas.lt>
2019-07-24 22:01:09 +0300

    more bootstrapping

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/notes';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -142,14 +142,40 @@
         </ul>
       </div>
 
-      <div class="col-md-7 border overflow-auto" style="height: 80vh">
-        <h2>Laba diena dienorasteli...</h2>
+      <div class="col-md-7 border overflow-auto bg-white" style="height: 80vh">
+
+        <h2>Card title</h2>
+
+        <p>
+          This is some text within a card body.
+        </p>
 
       </div>
 
     </div>
 
   </div>
+
+
+<div class="modal fade show" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="display: block">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 @endsection
 
