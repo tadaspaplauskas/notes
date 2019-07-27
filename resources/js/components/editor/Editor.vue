0d1280de28e2209c5221e6e80c3f4e3ad5d33b4b
Tadas <tadas@paplauskas.lt>
2019-07-27 16:07:14 +0300

    editor in progress

---
+++ resources/js/components/editor/Editor.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="editor">
+    <editor-menu-bar :editor="editor" v-slot="{ commands, isActive }">
+      <div class="menubar">
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>text-bold</title><path d="M17.194,10.962A6.271,6.271,0,0,0,12.844.248H4.3a1.25,1.25,0,0,0,0,2.5H5.313a.25.25,0,0,1,.25.25V21a.25.25,0,0,1-.25.25H4.3a1.25,1.25,0,1,0,0,2.5h9.963a6.742,6.742,0,0,0,2.93-12.786Zm-4.35-8.214a3.762,3.762,0,0,1,0,7.523H8.313a.25.25,0,0,1-.25-.25V3a.25.25,0,0,1,.25-.25Zm1.42,18.5H8.313a.25.25,0,0,1-.25-.25V13.021a.25.25,0,0,1,.25-.25h4.531c.017,0,.033,0,.049,0l.013,0h1.358a4.239,4.239,0,0,1,0,8.477Z"/></svg>
+
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
+        >
+          <icon name="italic" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+        >
+          <icon name="strike" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.underline() }"
+          @click="commands.underline"
+        >
+          <icon name="underline" />
+        </button>
+<!--
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+        >
+          <icon name="code" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.paragraph() }"
+          @click="commands.paragraph"
+        >
+          <icon name="paragraph" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 1 }) }"
+          @click="commands.heading({ level: 1 })"
+        >
+          H1
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+          @click="commands.heading({ level: 2 })"
+        >
+          H2
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+          @click="commands.heading({ level: 3 })"
+        >
+          H3
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bullet_list() }"
+          @click="commands.bullet_list"
+        >
+          <icon name="ul" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.ordered_list() }"
+          @click="commands.ordered_list"
+        >
+          <icon name="ol" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.blockquote() }"
+          @click="commands.blockquote"
+        >
+          <icon name="quote" />
+        </button>
+
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code_block() }"
+          @click="commands.code_block"
+        >
+          <icon name="code" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.horizontal_rule"
+        >
+          <icon name="hr" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.undo"
+        >
+          <icon name="undo" />
+        </button>
+
+        <button
+          class="menubar__button"
+          @click="commands.redo"
+        >
+          <icon name="redo" />
+        </button> -->
+
+      </div>
+    </editor-menu-bar>
+
+    <editor-content class="editor__content" :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from 'tiptap-extensions'
+
+export default {
+  components: {
+    EditorContent,
+    EditorMenuBar,
+  },
+  data() {
+    return {
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new BulletList(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new HorizontalRule(),
+          new ListItem(),
+          new OrderedList(),
+          new TodoItem(),
+          new TodoList(),
+          new Link(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Strike(),
+          new Underline(),
+          new History(),
+        ],
+        content: `
+          <h2>
+            Hi there,
+          </h2>
+          <p>
+            this is a very <em>basic</em> example of tiptap.
+          </p>
+          <pre><code>body { display: none; }</code></pre>
+          <ul>
+            <li>
+              A regular list
+            </li>
+            <li>
+              With regular items
+            </li>
+          </ul>
+          <blockquote>
+            It's amazing 👏
+            <br />
+            – mom
+          </blockquote>
+        `,
+      }),
+    }
+  },
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,151 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container" style="width: 80vw">
+  <div class="container" style="width: 100vw">
 
     <div class="row">
 
-      <div class="col-md-2 border overflow-auto" style="height: 80vh">
-        <h2>Tags</h2>
+      <div class="col-md-2 border overflow-auto bg-white" style="height: 80vh">
+        <h2 class="d-flex justify-content-between align-items-center">
+            Tags
+            <button class="btn btn-secondary btn-sm">+</button>
+        </h2>
 
-        <ul>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-        </ul>
+        <div class="list-group list-group-flush">
+          <button type="button" class="list-group-item  d-flex justify-content-between align-items-center active">
+            cons
+            <span class="badge badge-primary badge-pill">14</span>
+          </button>
+        </div>
+    </div>
+
+      <div class="col-md-4 border overflow-auto bg-white" style="height: 80vh">
+
+        <h2 class="d-flex justify-content-between align-items-center">
+            Notes
+            <button class="btn btn-secondary btn-sm">+</button>
+        </h2>
+
+        <div class="list-group list-group-flush">
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item</h5>
+              <small>3 days ago</small>
+            </div>
+            <small>Donec id elit non mi porta.</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item heading</h5>
+              <small class="text-muted">3 days ago</small>
+            </div>
+            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+            <small class="text-muted">Donec id elit non mi porta.</small>
+          </a>
+          <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+              <h5 class="mb-1">List group item heading</h5>
+              <small class="text-muted">3 days ago</small>
+            </div>
+            <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+            <small class="text-muted">Donec id elit non mi porta.</small>
+          </a>
+        </div>
+
       </div>
 
-      <div class="col-md-3 border overflow-auto" style="height: 80vh">
-        <h2>Notes</h2>
+      <div class="col-md-6 border overflow-auto bg-white" style="height: 80vh">
 
-        <ul>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-          <li>Laba</li>
-          <li>Diena</li>
-        </ul>
-      </div>
-
-      <div class="col-md-7 border overflow-auto bg-white" style="height: 80vh">
-
-        <h2>Card title</h2>
-
-        <p>
-          This is some text within a card body.
-        </p>
+        <editor></editor>
 
       </div>
 
