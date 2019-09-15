c7caaf3b1d0177c79ef28305c62143e8e0c81280
Tadas Paplauskas <tadas@paplauskas.lt>
2019-09-15 10:31:36 +0300

    files + delete link

--- resources/views/notes/form.blade.php
+++ resources/views/notes/form.blade.php
@@ -36,14 +36,22 @@
         {!! $note->html !!}
 
         @foreach ($note->uploads as $upload)
+        <div class="d-inline-block img-thumbnail m-1">
+          <a href="" class="close mx-2" aria-label="Delete" title="Delete file">
+            <span aria-hidden="true">&times;</span>
+          </a>
+
           <a href="{{ $upload->url }}">
             @if ($upload->is_image)
               <img src="{{ $upload->url }}" title="{{ $upload->name }}"
-                class="img-fluid img-thumbnail" style="height: 10rem"">
+                class="img-fluid" style="max-height: 10rem">
             @else
               {{ $upload->name }}
             @endif
           </a>
+
+
+        </div>
         @endforeach
       </div>
     @endif
