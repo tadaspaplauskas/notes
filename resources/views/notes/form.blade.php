5298abcf83d97f3ac612f34f96d613a77e53f893
Tadas Paplauskas <tadas@paplauskas.lt>
2019-09-15 11:58:19 +0300

    this nicer yas

--- resources/views/notes/form.blade.php
+++ resources/views/notes/form.blade.php
@@ -36,7 +36,7 @@
         {!! $note->html !!}
 
         @foreach ($note->uploads as $upload)
-        <div class="d-inline-block img-thumbnail m-1">
+        <div class="img-thumbnail m-3">
           <a href="{{ route('uploads.delete', $upload) }}" class="close mx-2" aria-label="Delete" title="Delete file">
             <span aria-hidden="true">&times;</span>
           </a>
@@ -44,7 +44,7 @@
           <a href="{{ $upload->url }}">
             @if ($upload->is_image)
               <img src="{{ $upload->url }}" title="{{ $upload->name }}"
-                class="img-fluid" style="max-height: 10rem">
+                class="img-fluid">
             @else
               {{ $upload->name }}
             @endif
