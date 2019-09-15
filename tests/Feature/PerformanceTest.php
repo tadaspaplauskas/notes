e1b9c3bd2f4a21cc2121a5cfdbd5371ede8a875d
Tadas Paplauskas <tadas@paplauskas.lt>
2019-09-15 15:10:11 +0300

    Performance tests for db

---
+++ tests/Feature/PerformanceTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Note;
+
+class PerformanceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testDBPerformance()
+    {
+        factory(Note::class, 10000)->create(['user_id' => '70E2E8DE-500E-4630-B3CB-166131D35C21']);
+
+        $theOne = factory(Note::class)->create(['user_id' => '70E2E8DE-500E-4630-B3CB-166131D35C21']);
+
+        factory(Note::class, 20000)->create(['user_id' => '70E2E8DE-500E-4630-B3CB-166131D35C21']);
+
+        $this->assertNotNull(Note::find($theOne->id));
+    }
+}
