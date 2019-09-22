8dad75c4554f9f0e5f1e6bb6e657c476d970bf0c
Tadas Paplauskas <tadas@paplauskas.lt>
2019-09-22 11:34:47 +0300

    fixing routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return \Auth::check() ?
+        redirect()->route('notes.index') : redirect('/login');
 });
 
 Auth::routes();
