03c88d0cc54f803c34f02330e9da17ff49b09bff
Tadas Paplauskas <tadas@paplauskas.lt>
2019-09-22 11:28:24 +0300

    move out benchmark out of suite. Can be run individually

--- tests/Feature/PerformanceBenchmark.php
+++ tests/Feature/PerformanceBenchmark.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Note;
 
-class PerformanceTest extends TestCase
+class PerformanceBenchmark extends TestCase
 {
     use RefreshDatabase;
 
@@ -18,7 +18,6 @@ class PerformanceTest extends TestCase
      */
     public function testDBPerformance()
     {
-        $this->markTestSkipped();
         factory(Note::class, 10000)->create(['user_id' => '70E2E8DE-500E-4630-B3CB-166131D35C21']);
 
         $theOne = factory(Note::class)->create(['user_id' => '70E2E8DE-500E-4630-B3CB-166131D35C21']);
