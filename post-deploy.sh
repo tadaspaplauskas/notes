306d09a92ab21de615c15a02e6be33293f45b122
Tadas Paplauskas <tadas@paplauskas.lt>
2019-09-22 12:13:29 +0300

    post deploy script update, npm run prod

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 composer install
+npm install
+npm run prod
 php artisan migrate --force
-php artisan route:cache
 php artisan cache:clear
 php artisan view:clear
 php artisan config:cache
