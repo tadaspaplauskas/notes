4058a8a1a471eb53302a71359ef5877459e0d104
Tadas Paplauskas <tadas@paplauskas.lt>
2019-10-20 11:51:48 +0300

    delete image only where applicable

--- resources/views/notes/content.blade.php
+++ resources/views/notes/content.blade.php
@@ -2,9 +2,12 @@
 
 @foreach ($note->uploads as $upload)
 <div class="img-thumbnail my-3">
-  <a href="{{ route('uploads.delete', $upload) }}" class="close mx-2" aria-label="Delete" title="Delete file">
-    <span aria-hidden="true">&times;</span>
-  </a>
+
+  @if (Route::current()->getName() == 'notes.edit')
+    <a href="{{ route('uploads.delete', $upload) }}" class="close mx-2" aria-label="Delete" title="Delete file">
+      <span aria-hidden="true">&times;</span>
+    </a>
+  @endif
 
   <a href="{{ $upload->url }}">
     @if ($upload->is_image)
